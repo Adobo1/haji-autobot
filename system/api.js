b5318d0f93971278d42cd648671c87cc549d59c5
deva69 <148951486+Adobo1@users.noreply.github.com>
2024-12-16 17:12:45 +0000

    1

--- system/api.js
+++ system/api.js
@@ -1,5 +1,5 @@
 module.exports = {
-  markdevs69: "https://markdevs-last-api-s7d0.onrender.com",
+  markdevs69: "https://markdevs-last-api-z7p7.onrender.com",
   markdevs69v2: "https://apis-markdevs69v2-cq4u.onrender.com",
   kim: "https://hiroshi-rest-api.replit.app",
   liaspark: "https://liaspark.chatbotcommunity.ltd",
